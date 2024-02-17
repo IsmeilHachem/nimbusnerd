0611f11a54fe512b86db415fd9b856fca9f7ec7f
Ismeil Hachem <ismeil27@gmail.com>
2024-02-17 18:47:18 -0500

    added back button, nav bar, and css for them

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const MarkdownIt = require('markdown-it');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const MarkdownIt = require("markdown-it");
 const md = new MarkdownIt();
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  fs.readdir(path.join(__dirname, '/blogs/'), (err, files) => {
+app.get("/", (req, res) => {
+  fs.readdir(path.join(__dirname, "/blogs/"), (err, files) => {
     if (err) {
       console.error(err);
       return res.sendStatus(500);
     }
-    let links = files.map(file => {
-      const name = path.basename(file, '.md');
-      return `<a href="/blog/${name}">${name}</a>`;
-    }).join('<br>');
+    let links = files
+      .map((file) => {
+        const name = path.basename(file, ".md");
+        return `<a href="/blog/${name}">${name}</a>`;
+      })
+      .join("<br>");
     const html = `
       <!DOCTYPE html>
       <html>
@@ -26,6 +28,11 @@ app.get('/', (req, res) => {
         <link rel="stylesheet" type="text/css" href="/styles.css">
       </head>
       <body>
+        <div class="navbar">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </div>
         <div class="container">
           <h1>Nimbus Nerds, Unite!</h1>
           ${links}
@@ -37,31 +44,36 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/blog/:name', (req, res) => {
+app.get("/blog/:name", (req, res) => {
   const fileName = req.params.name;
-  fs.readFile(path.join(__dirname, '/blogs/', fileName + '.md'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.sendStatus(500);
+  fs.readFile(
+    path.join(__dirname, "/blogs/", fileName + ".md"),
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.sendStatus(500);
+      }
+      const result = md.render(data);
+      const html = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <link rel="stylesheet" type="text/css" href="/styles.css">
+        </head>
+        <body>
+          <div class="container">
+            <a href="/" class="back-button">Back</a>
+            ${result}
+          </div>
+        </body>
+        </html>
+      `;
+      res.send(html);
     }
-    const result = md.render(data);
-    const html = `
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <link rel="stylesheet" type="text/css" href="/styles.css">
-      </head>
-      <body>
-        <div class="container">
-          ${result}
-        </div>
-      </body>
-      </html>
-    `;
-    res.send(html);
-  });
+  );
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running at http://localhost:${port}`)
+  console.log(`Server running at http://localhost:${port}`);
 });
